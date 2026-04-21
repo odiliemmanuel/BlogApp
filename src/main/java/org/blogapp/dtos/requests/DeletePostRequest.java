264834bb64d1c4262112d35c86f54c0d9c78f464
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-21 06:41:37 +0100

    "blog"

--- src/main/java/org/blogapp/dtos/requests/DeletePostRequest.java
+++ src/main/java/org/blogapp/dtos/requests/DeletePostRequest.java
@@ -1,4 +1,11 @@
 package org.blogapp.dtos.requests;
 
+import lombok.Data;
+
+@Data
 public class DeletePostRequest {
+
+    private String userId;
+    private String postId;
+
 }
--- src/main/java/org/blogapp/dtos/responses/DeletePostResponse.java
+++ src/main/java/org/blogapp/dtos/responses/DeletePostResponse.java
@@ -1,4 +1,9 @@
 package org.blogapp.dtos.responses;
 
+import lombok.Data;
+
+@Data
 public class DeletePostResponse {
+
+    String message;
 }
